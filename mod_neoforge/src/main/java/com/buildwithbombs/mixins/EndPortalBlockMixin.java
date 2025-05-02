65093c219e8c97c556d9f51b353a54fbd14ffc67
Timothy Barnes <timothyb2357@gmail.com>
2025-05-02 10:50:25 -0700

    Created end portal mixin

---
+++ mod_neoforge/src/main/java/com/buildwithbombs/mixins/EndPortalBlockMixin.java
@@ -0,0 +1,20 @@
+package com.buildwithbombs.mixins;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.EndPortalBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(EndPortalBlock.class)
+public class EndPortalBlockMixin {
+    @Inject(method = "entityInside", at = @At("HEAD"), cancellable = true)
+
+    private void onEntityInside(BlockState state, Level level, BlockPos pos, Entity entity, CallbackInfo ci) {
+        ci.cancel();
+    }
+}
